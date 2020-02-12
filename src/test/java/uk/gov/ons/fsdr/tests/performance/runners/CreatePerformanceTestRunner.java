27619b4861e9cbb29d8b3e92f96f199860a50424
jacobharrison <jacobharrison@outlook.com>
2020-02-12 11:42:13 +0000

    added annotations to runner

--- src/test/java/uk/gov/ons/fsdr/tests/performance/runners/CreatePerformanceTestRunner.java
+++ src/test/java/uk/gov/ons/fsdr/tests/performance/runners/CreatePerformanceTestRunner.java
@@ -3,10 +3,14 @@ package uk.gov.ons.fsdr.tests.performance.runners;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "html:target/cucumber-reports"},
     features = {"src/test/resources/acceptancetests/createPerformance.feature"},
     glue = {"uk.gov.ons.fsdr.tests.performance.steps"})
+@Configuration
+@ComponentScan({"uk.gov.census.ffa.storage.utils"})
 public class CreatePerformanceTestRunner {
 }
